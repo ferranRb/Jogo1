85e5e520c6b25db5887c72f987c0b2d8a578aaa2
ferranRb <127503979+ferranRb@users.noreply.github.com>
2023-03-13 13:39:31 +0100

    Initial Commit

---
+++ Rey del Salto.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+var velocitat := 500
+var moviment := Vector2.ZERO
+var gravetat := Vector2.DOWN * 950
+var salt := Vector2.UP * 600
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	moviment += gravetat * delta
+	if Input.is_action_just_pressed("salta") and is_on_floor():
+		moviment += salt
+	
+	if is_on_floor():
+		moviment.x = Input.get_axis("ves_esquerra","ves_dreta") * velocitat
+	moviment = move_and_slide(moviment, Vector2.UP)
+	
+	
+#	var direccio := Vector2.ZERO
+#	if Input.is_action_pressed("ves_dreta"):
+#		direccio += Vector2.RIGHT
+#	if Input.is_action_pressed("ves_esquerra"):
+#		direccio += Vector2.LEFT
+#	if Input.is_action_pressed("ves_amunt"):
+#		direccio += Vector2.UP 
+
+	
